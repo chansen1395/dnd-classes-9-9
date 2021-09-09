fc81d7b3aad8e79b2d459330bb2c2c3e6a09b969
Connor Hansen <chansen13@georgefox.edu>
2021-09-09 14:49:44 -0700

    add working d&d class selector. Buggy when making API calls to specific classes.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,53 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+$(document).ready(function () {
+  $('#classSubmit').click(function () {
+    $('.showSkills, .showProfs, .showDndClass, .showSpells').empty();
+    const classSelection = $('#class-selection').val();
+    $('#class-selection').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `https://www.dnd5eapi.co/api/classes/${classSelection}`;
 
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
-    }
+    };
 
     request.open("GET", url, true);
     request.send();
 
+    // Relay information to user
     function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showDndClass').append(`<h3>Info for ${response.name} class</h3>`);
+      $('.showDndClass').append(`<li>Hit Die: ${response.hit_die}</li><hr>`);
+      $('.showDndClass').append(`<li>Starting Equipment example: ${response.starting_equipment_options[0].from[0].equipment.name}</li><hr>`);
+
+      let skillCount = response.proficiency_choices[0].from.length;
+      let skillSelect = 0;
+      // console.log(i);
+      // console.log(response.proficiency_choices[0].from[0].name);
+      while (skillCount >= 1) {
+        $('.showSkills').append(`<li>Skill ` + (skillSelect + 1) + `: ${response.proficiency_choices[0].from[skillSelect].name.replace("Skill:", "")}</li>`);
+        skillCount--;
+        skillSelect++;
+      }
+
+      let profCount = response.proficiencies.length;
+      let profSelect = 0;
+      while (profCount >= 1) {
+        $('.showProfs').append(`<li>Proficiency ` + (profSelect + 1) + `: ${response.proficiencies[profSelect].name}</li>`);
+        profCount--;
+        profSelect++;
+      }
+
+      $('.showSpells').append(`<li>${response.spellcasting.info[0].desc}</li>`);
+      $('.showSpells').append(`<li>${response.spellcasting.info[1].desc}</li>`);
+      $('.showSpells').append(`<li>THIS CAN BE USED TO NEST API CALLS: ${response.spells}</li>`);
     }
   });
+
 });
